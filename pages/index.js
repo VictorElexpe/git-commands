27cda62981dd133da78dc174caed67ed442f5e79
victorelexpe <victorelexpe@gmail.com>
2020-05-28 22:23:29 +0200

    First commit

--- pages/index.js
+++ pages/index.js
@@ -4,46 +4,43 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Learn about how Git works</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn about how Git works!
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Git helps you and your team track and organize the changes you make in your projects.
         </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
+            <h3><code>git init</code></h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
+            <h3><code>git clone</code></h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://nextjs.org/learn" className="card">
+            <h3><code>git add</code></h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://nextjs.org/learn" className="card">
+            <h3><code>git commit</code></h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className="card">
+            <h3><code>git push</code></h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
         </div>
       </main>
@@ -64,6 +61,8 @@ export default function Home() {
           min-height: 100vh;
           padding: 0 0.5rem;
           display: flex;
+          background-color: #000;
+          color: #FFF;
           flex-direction: column;
           justify-content: center;
           align-items: center;
@@ -125,12 +124,13 @@ export default function Home() {
         }
 
         .description {
+          max-width: 50%;
           line-height: 1.5;
           font-size: 1.5rem;
         }
 
         code {
-          background: #fafafa;
+          background: #afafaf;
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
